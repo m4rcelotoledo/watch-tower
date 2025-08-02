8ab1bbaf0eabec520593e9858294c697f3cc90df
Marcelo Toledo <marcelotoledo5000@gmail.com>
2025-08-02 11:17:02 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2020_04_11_174428) do
+ActiveRecord::Schema[8.0].define(version: 2020_04_11_174428) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "appointments", force: :cascade do |t|
     t.bigint "visitor_id", null: false
