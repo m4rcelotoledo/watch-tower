570168817cc675184ecbadc9d142a4e84f2f3553
Marcelo Toledo <marcelotoledo5000@gmail.com>
2026-06-03 14:06:33 -0300

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,7 @@
 
 class SessionsController < Devise::SessionsController
   respond_to :json
+
+  # JWT auth is stateless; warden has no session user on logout even with a valid token.
+  skip_before_action :verify_signed_out_user, only: :destroy
 end
