1e69e8cdc08bdfa2fca06f998c73e0a48c3a2cec
Marcelo Toledo <marcelotoledo5000@gmail.com>
2026-06-03 14:09:44 -0300

    Update sessions_controller.rb
    Fix rubocop offense

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,8 @@ class SessionsController < Devise::SessionsController
 
   # JWT auth is stateless; warden has no session user on logout even with a valid token.
   skip_before_action :verify_signed_out_user, only: :destroy
+
+  def destroy
+    super
+  end
 end
